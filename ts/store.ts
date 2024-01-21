bfe8e4e85c16cb239023b38093624716eec90263
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-01-21 20:18:48 +0100

    Continue directory grid, add ArcTerm Scripts folder

--- ts/store.ts
+++ ts/store.ts
@@ -1,4 +1,4 @@
-import { DocumentsFolderIcon, ThemeFolderIcon, WallpapersFolderIcon } from "$ts/images/filesystem";
+import { ArcTermFolderIcon, DocumentsFolderIcon, ThemeFolderIcon, WallpapersFolderIcon } from "$ts/images/filesystem";
 import { SystemFolder } from "./types";
 
 export const SystemFolders: SystemFolder[] = [
@@ -6,6 +6,11 @@ export const SystemFolders: SystemFolder[] = [
     name: "Documents",
     path: "./Documents",
     icon: DocumentsFolderIcon,
+  },
+  {
+    name: "Scripts",
+    path: "./Scripts",
+    icon: ArcTermFolderIcon
   },
   {
     name: "Themes",
